56c3d9d9263c43a028bf6de38834ce642907bdf3
sspowers <sspowers@rgmiller.stanford.edu>
2015-02-20 12:15:32 -0800

    updates to sims 9 and 10

--- code/fig-simulation.r
+++ code/fig-simulation.r
@@ -254,7 +254,7 @@ matplot(log(2^(1:12)), resultsMean[, 1:5*4-3], type = 'l', xlab = 'Dimension',
     ylab = 'Rejection rate', main = 'Rejection rate on old classes', axes = F)
 axis(1, log(2^(1:12)), 2^(1:12))
 axis(2)
-legend('bottomright',
+legend('topleft', legend =
     c('a=.05', 'a=.05,b=.45', 'a=.05,b=.43', 'a=.05,b=.5', 'a=.05,b=.4'),
     col = 1:5, lty = 1:5)
 matplot(log(2^(1:12)), resultsMean[, 1:5*4-2], type = 'l', xlab = 'Dimension',
@@ -267,7 +267,7 @@ pdf('figs/simulation-9-coverage.pdf')
 matplot(log(2^(1:12)), resultsMean[, 1:5*4-1], type = 'l', xlab = 'Dimension',
     ylab = 'Coverage rate', main = 'Coverage rate on original classes',
     axes = FALSE)
-legend('bottomright',
+legend('bottomleft', legend =
     c('a=.05', 'a=.05,b=.45', 'a=.05,b=.43', 'a=.05,b=.5', 'a=.05,b=.4'),
     col = 1:5, lty = 1:5)
 axis(1, log(2^(1:12)), 2^(1:12))
@@ -275,5 +275,30 @@ axis(2, c(.8, .95), c(.8, .95))
 dev.off()
 
 
+# tenth simulation
+range = 110:119
+load(paste('data/solomon/simulation', range[1], '.RData', sep = ''))
+resultsTotal = array(NA, dim = c(nrow(results), ncol(results), length(range)))
+for (i in range) {
+    load(paste('data/solomon/simulation', i , '.RData', sep = ''))
+    resultsTotal[, , i + 1 - min(range)] = as.matrix(results)
+}
+resultsMean = apply(resultsTotal, 1:2, mean)
+
+pdf('figs/simulation-10.pdf', width = 14)
+plot(resultsMean[, 1], resultsMean[, 2], type = 'l',
+    xlim = c(0, 1), xlab = 'Rejection rate on original classes',
+    ylim = c(0, 1), ylab = 'Rejection rate on new class')
+legend('bottomright', legend = 1:5, col = 1:5, lty = 1:5)
+for (i in 1:4) {
+    lines(resultsMean[, 4*i+1], resultsMean[, 4*i+2], col = i, lty = i)
+}
+dev.off()
+
+pdf('figs/simulation-10-coverage.pdf')
+matplot(0:20/20, resultsMean[, 1:5*4-1], type = 'l', xlab = 'alpha',
+    ylab = 'Coverage rate', main = 'Original classes')
+legend('topleft', legend = 1:5, col = 1:5, lty = 1:5)
+dev.off()
 
 
